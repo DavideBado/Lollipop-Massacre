b5c02fb9c5c0081b1e255443658bf0db76054511
Davide <davidebadolato@tiscali.it>
2019-03-28 01:48:14 +0100

    Aggiunta preview Drain

--- Assets/Scripts/Abilities/Drain.cs
+++ Assets/Scripts/Abilities/Drain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Drain : MonoBehaviour
 {
@@ -101,6 +102,147 @@ public class Drain : MonoBehaviour
             }
         }
 
+    }
+
+    public void Preview()
+    {
+
+        if (GetComponent<Agent>().MyTurn && GetComponent<Agent>().PlayerType == 2 && GetComponent<Agent>().ImStunned == false && Manager.CanAttack == true)
+
+        {
+            CleanPreview();
+            float _lookX = GetComponent<Agent>().SavedlookAt.x;
+            float _lookY = GetComponent<Agent>().SavedlookAt.z;
+            Vector3 playerPosition = transform.position;
+
+            List<CellPrefScript> cells = new List<CellPrefScript>();
+
+            cells = FindObjectsOfType<CellPrefScript>().ToList();
+
+            RaycastHit hit;
+
+            if (GetComponent<LifeManager>().Life == 6)
+            {
+                if (Physics.Raycast(GetComponent<Agent>().RayCenter + new Vector3(0, 0.5f), GetComponent<Agent>().SavedlookAt, out hit, Mathf.Infinity))
+                {
+                    Debug.DrawRay(GetComponent<Agent>().RayCenter + new Vector3(0, 0.5f), GetComponent<Agent>().SavedlookAt * hit.distance, Color.black);
+                    if (_lookX != 0)
+                    {
+                        CellsGreenInRay(hit.transform.position, cells, playerPosition);
+                    }
+                    else if (_lookY != 0)
+                    {
+                        CellsGreenInRay(hit.transform.position, cells, playerPosition);
+
+                    }
+
+                }
+                else
+                {
+
+                    if (_lookX != 0)
+                    {
+                        if (_lookX > 0)
+                        {
+                            CellsGreenInRay(new Vector3(GetComponent<Agent>().configGrid.DimX, 0, transform.position.z), cells, playerPosition);
+                        }
+                        else if (_lookX < 0)
+                        {
+                            CellsGreenInRay(new Vector3(-1, 0, transform.position.z), cells, playerPosition);
+                        }
+
+                    }
+                    else if (_lookY != 0)
+                    {
+
+                        if (_lookY < 0)
+                        {
+                            CellsGreenInRay(new Vector3(transform.position.x, 0, -1), cells, playerPosition);
+                        }
+                        else if (_lookY > 0)
+                        {
+                            CellsGreenInRay(new Vector3(transform.position.x, 0, GetComponent<Agent>().configGrid.DimY), cells, playerPosition);
+                        }
+
+                    }
+                }
+            }
+
+            if (GetComponent<LifeManager>().Life < 6)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0, 0.5f), Vector3.forward, out hit, 4))
+                {
+                    CellsGreenInRay(hit.transform.position, cells, playerPosition);
+                }
+                else
+                {
+                    CellsGreenInRay(new Vector3(transform.position.x, 0, (transform.position.z + 5)), cells, playerPosition);
+                }
+
+                if (Physics.Raycast(transform.position + new Vector3(0, 0.5f), Vector3.back, out hit, 4))
+                {
+                    CellsGreenInRay(hit.transform.position, cells, playerPosition);
+                }
+                else
+                {
+                    CellsGreenInRay(new Vector3(transform.position.x, 0, (transform.position.z - 5)), cells, playerPosition);
+                }
+
+                if (Physics.Raycast(transform.position + new Vector3(0, 0.5f), Vector3.right, out hit, 4))
+                {
+                    CellsGreenInRay(hit.transform.position, cells, playerPosition);
+                }
+                else
+                {
+                    CellsGreenInRay(new Vector3((transform.position.x + 5), 0, transform.position.z), cells, playerPosition);
+                }
+
+                if (Physics.Raycast(transform.position + new Vector3(0, 0.5f), Vector3.left, out hit, 4))
+                {
+                    CellsGreenInRay(hit.transform.position, cells, playerPosition);
+                }
+                else
+                {
+                    CellsGreenInRay(new Vector3((transform.position.x - 5), 0, transform.position.z), cells, playerPosition);
+                }
+            }
+        }
+    }
+
+    public void CleanPreview()
+    {
+        List<CellPrefScript> cells = new List<CellPrefScript>();
+
+        cells = FindObjectsOfType<CellPrefScript>().ToList();
+
+        foreach (CellPrefScript cell in cells)
+        {
+            cell.GetComponentInParent<CellPrefScript>().Color = cell.GetComponentInParent<CellPrefScript>().BaseColor;
+        }
+    }
+
+    void CellsGreenInRay(Vector3 HitPosition, List<CellPrefScript> cells, Vector3 playerPosition)
+    {
+        foreach (CellPrefScript cell in cells)
+        {
+            if ((((playerPosition.x < cell.transform.position.x && cell.transform.position.x < HitPosition.x)
+
+                ||
+
+               (playerPosition.x > cell.transform.position.x && cell.transform.position.x > HitPosition.x)) &&
+
+
+               (cell.transform.position.z == HitPosition.z)) ||
+
+
+
+               (((playerPosition.z < cell.transform.position.z && cell.transform.position.z < HitPosition.z) ||
+               (playerPosition.z > cell.transform.position.z && cell.transform.position.z > HitPosition.z)) &&
+               (cell.transform.position.x == HitPosition.x)))
+            {
+                cell.GetComponentInParent<CellPrefScript>().Color = Color.green;
+            }
+        }
     }
 
 }
