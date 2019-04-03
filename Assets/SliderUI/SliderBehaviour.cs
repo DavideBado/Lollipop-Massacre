5999f597e527b1dadae016aae913cd86b80f1c65
Davide <davidebadolato@tiscali.it>
2019-04-03 21:25:48 +0200

    Aggiunte immagini per schermata selezione personaggio

--- Assets/SliderUI/SliderBehaviour.cs
+++ Assets/SliderUI/SliderBehaviour.cs
@@ -8,30 +8,34 @@ using System;
 public class SliderBehaviour : MonoBehaviour
 {
     private Sequence sequenceTimer;
+    private Sequence sequenceFill;
     public Image fillImage;
     public TMPro.TextMeshProUGUI counterText;
     public bool Loop = false;
     public bool StartOnAwake = false;
     [SerializeField] private float countAmount = 5f;
+    float m_StopFill;
     public Color RingColor;
 
     public delegate void TimerDelegate();
 
     public TimerDelegate StartTimer;
     public TimerDelegate EndTimer;
-    public GameManager manager;
+    GameManager manager;
     public float endValue = 0;
 
 
-    public void Start() {
-        if (StartOnAwake)
-            StartCounter(countAmount);
-
-        if (Loop)
-            StartCounter(countAmount);
-
+    public void Start()
+    {
+        m_StopFill = countAmount;
         fillImage.color = RingColor;
         manager = FindObjectOfType<GameManager>();
+        if (StartOnAwake)
+            StartCounter();
+
+        if (Loop)
+            StartCounter();
+
     }
 
    
@@ -44,30 +48,36 @@ public class SliderBehaviour : MonoBehaviour
         //}
 
         counterText.text = ((int)countAmount + 1).ToString();
+
         if (manager.TimerOn == false)
         {
+
+            m_StopFill = -100000;
             sequenceTimer.Pause();
         }
         else
         {
+            m_StopFill = countAmount;
             sequenceTimer.Play();
+           
+
+
         }
     }
 
-    public float initialCountAmount;
+    float initialCountAmount = 5f;
 
-    public void StartCounter(float time) {
-        initialCountAmount = time;
-       
-            fillImage.DOFillAmount(0, 1 / countAmount)
+    public void StartCounter() {
+
+
+           fillImage.DOFillAmount(0, 1 / countAmount)
            .SetSpeedBased(true)
            .SetEase(Ease.Linear);
 
 
           sequenceTimer = DOTween.Sequence().Append(DOTween.To(() => countAmount, x => countAmount = x, endValue, countAmount)
             .SetEase(Ease.Linear)
-            .OnComplete(timerCompleted));
-        
+            .OnComplete(timerCompleted));   
        
         
         
@@ -76,11 +86,12 @@ public class SliderBehaviour : MonoBehaviour
     public void timerCompleted() {
         fillImage.DOFillAmount(1, 0);
         countAmount = initialCountAmount;
-        if(EndTimer != null)
+       
+        if (EndTimer != null)
             EndTimer();
         if (Loop)
         {
-            StartCounter(countAmount);
+            StartCounter();
         }
     }
 
