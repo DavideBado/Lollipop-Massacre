a4107ca8faab0060cf47b9975855b05fcb1655a3
Davide <davidebadolato@tiscali.it>
2019-04-10 16:26:35 +0200

    Aggiornati colori Venom e Stun

--- Assets/Scripts/Level Manager/CellPrefScript.cs
+++ Assets/Scripts/Level Manager/CellPrefScript.cs
@@ -20,25 +20,25 @@ public class CellPrefScript : MonoBehaviour
         return itemData;
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.GetComponent<PlayerData>() != null)
-    //    {
-    //        other.transform.parent = transform.parent;
-    //        if (other.GetComponent<Agent>().PlayerID == 1)
-    //        {
-    //            GetComponent<MeshRenderer>().material = Materials[1];
-    //        }
-    //        else if (other.GetComponent<Agent>().PlayerID == 2)
-    //        {
-    //            GetComponent<MeshRenderer>().material = Materials[2];
-    //        }
-    //    }
-    //    //else
-    //    //{
-    //    //    GetComponent<MeshRenderer>().material = Materials[0];
-    //    //}
-    //}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerData>() != null)
+        {
+            other.transform.parent = transform.parent;
+            //if (other.GetComponent<Agent>().PlayerID == 1)
+            //{
+            //    GetComponent<MeshRenderer>().material = Materials[1];
+            //}
+            //else if (other.GetComponent<Agent>().PlayerID == 2)
+            //{
+            //    GetComponent<MeshRenderer>().material = Materials[2];
+            //}
+        }
+        //else
+        //{
+        //    GetComponent<MeshRenderer>().material = Materials[0];
+        //}
+    }
 
     //private void OnTriggerExit(Collider other)
     //{
