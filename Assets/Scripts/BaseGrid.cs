7042260a314a14025e89422930e5231f3fbf6913
ebasile <edoardo.basile96@gmail.com>
2019-05-14 12:40:53 +0200

    risolti ulteriori errori

--- Assets/Scripts/BaseGrid.cs
+++ Assets/Scripts/BaseGrid.cs
@@ -6,33 +6,33 @@ namespace GridSystem {
 
     public class BaseGrid : MonoBehaviour {
 
+        
+        public GridConfigData ConfigData;
         [SerializeField]
-        private GridConfigData configData;
-
-        public GameObject CellPrefab;
+        protected GameObject CellPrefab;
 
         protected List<Cell> Cells = new List<Cell>();
-
+        List<GameObject> Objects = new List<GameObject>();
+       
         void Start() {
-            CreateGrid(configData);
+            CreateGrid(ConfigData);
+
         }
 
         #region API
 
         public void CreateGrid(GridConfigData _configData) {
             // iterazione per la dimensione X della griglia
-            for (int x = 0; x < configData.DimX; x++) {
-                for (int y = 0; y < configData.DimY; y++) {
-                    
+            for (int x = 0; x < _configData.DimX; x++) {
+                // iterazione per la dimensione Y della griglia
+                for (int y = 0; y < _configData.DimY; y++) {
                     Cell cellToAdd = new Cell(x, y, new Vector3(
-                        transform.position.x + (x * configData.CellDim),
+                        transform.position.x + (x * _configData.CellDim),
                         0,
-                        transform.position.z + (y * configData.CellDim)
+                        transform.position.z + (y * _configData.CellDim)
                         ));
-
                     Cells.Add(cellToAdd);
-                    // Debug
-                    Instantiate(CellPrefab, cellToAdd.worldPosition, Quaternion.identity);
+                    GameObject _Cell = Instantiate(CellPrefab, cellToAdd.worldPosition, Quaternion.identity);
                 }
             }
         }
@@ -43,11 +43,54 @@ namespace GridSystem {
                     return cell.worldPosition;
                 }
             }
-
             return Vector3.zero;
         }
 
-        #endregion
+        public Cell GetCell(int _x, int _y)
+        {
+            foreach (Cell cell in Cells)
+            {
+                if (cell.x == _x && cell.y == _y)
+                {
+                    return cell;
+                }
+            }
+            return null;
+        }
 
-    }
-}
+        #endregion
+        private void Update()
+        {
+            ManagerCell();
+        }
+        public void ManagerCell()
+        {
+            GameObject[] PlayerList = GameObject.FindGameObjectsWithTag("Player");
+            GameObject[] WallList = GameObject.FindGameObjectsWithTag("Wall");
+
+           
+           
+           
+            foreach (Cell cell in Cells)
+            {
+                foreach (GameObject Object in PlayerList)
+                {
+                    if (cell.worldPosition == Object.transform.position)
+                    {
+                        cell.Free = false;
+                      //  Debug.Log("X:" + cell.x + " Y" + cell.y + " Free:" + cell.Free);
+                    }
+                    else cell.Free = true;
+                }
+                //foreach (GameObject Object in WallList)
+                //{
+                //    if (cell.worldPosition == Object.transform.position)
+                //    {
+                //        cell.Free = false;
+                //    }
+                //    else cell.Free = true;
+                //}
+               
+            }
+        }
+    }}
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,24 +4,33 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace GridSystem {
+namespace GridSystem
+{
 
-    public class Cell {
-
+    public class Cell
+    {
+        public bool Free;
         internal int x;
         internal int y;
         internal Vector3 worldPosition;
 
-        public Cell() {
+        public Cell()
+        {
 
         }
-
-        public Cell(int _x, int _y, Vector3 _worldPosition) {
+        
+        public Cell(int _x, int _y, Vector3 _worldPosition)
+        {
             x = _x;
             y = _y;
             worldPosition = _worldPosition;
         }
+        public bool CellFree()
+        {
 
+            return Free;
+        }
     }
-
 }
+    
+
