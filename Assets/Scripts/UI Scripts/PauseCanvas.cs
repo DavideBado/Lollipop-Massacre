967964ba12ad82fae21d3818323166c87b2292fd
ebasile96 <edoardo.basile96@gmail.com>
2019-06-18 18:14:47 +0200

    aggiunta UI pausa, manca bottone commands

--- Assets/Scripts/UI Scripts/PauseCanvas.cs
+++ Assets/Scripts/UI Scripts/PauseCanvas.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseCanvas : MonoBehaviour
 {
     GameManager manager;
-
     private void Start()
     {
         manager = FindObjectOfType<GameManager>();
+
+
     }
     private void OnDisable()
     {
 
         manager.MyPause();
     }
+
 }
---
+++ Assets/Scripts/UI Scripts/pausemmbutton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class pausemmbutton : Button
+{
+    public EventSystem eventsys;
+    BaseEventData baseEvent;
+    
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+
+        GetComponent<OnSelectData>().PanelActive.SetActive(true);
+        GetComponent<OnSelectData>().panelBase.SetActive(false);
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        GetComponent<OnSelectData>().panelBase.SetActive(true);
+        GetComponent<OnSelectData>().PanelActive.SetActive(false);
+    }
+
+    public override void OnSubmit(BaseEventData eventData)
+    {
+
+        GetComponent<MyEventSystemProvider>().eventSystem.SetSelectedGameObject(GetComponent<OnSelectData>().FirstSelectable);
+        GetComponent<OnSelectData>().ThisCanvas.SetActive(false);
+        scenechange();
+
+    }
+
+    void scenechange()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
