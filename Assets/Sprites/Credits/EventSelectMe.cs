cf4ea369b4d17c5d70c08d498bf95c97fc3108c7
Davide <davidebadolato@tiscali.it>
2019-07-14 16:36:59 +0200

    Aggiunta schermata credits

---
+++ Assets/Sprites/Credits/EventSelectMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSelectMe : MonoBehaviour
+{
+    public EventSystemCustom EventSystem;
+
+    private void OnEnable()
+    {
+        EventSystem.UpdateEventSystem(gameObject);
+    }
+}
---
+++ Assets/Sprites/Credits/GotoMainMenuFastFix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class GotoMainMenuFastFix : Button
+{
+    public override void OnSubmit(BaseEventData eventData)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
